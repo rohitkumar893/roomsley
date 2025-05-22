bc3feca467777b7a938c7e1bf631521ed268e555
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-05-22 08:25:45 +0530

    changed font weight of slogan

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ function App() {
             <div className='marginctrl flex flex-col md:flex-row h-full w-full justify-between gap-[45px]'>
               <div className='flex flex-col gap-[30px]'>
                   <h1 className='textlogo text-[50px] w-[300px] md:text-[60px] font-bold leading-tight tracking-wide'>Find Perfect<br />Room<span className='animate-pulse'>.</span></h1>
-                  <h2 className='text-[18px] sm:w-[410px] w-[360px] font-medium'> Whether you're finding a place or offering one, Roomsley makes it simple and quick.</h2>
+                  <h2 className='text-[18px] sm:w-[410px] w-[360px] font-normal'> Whether you're finding a place or offering one, Roomsley makes it simple and quick.</h2>
                   <Link to="/listings">
                   <button className="buttonn w-[180px]">
                   <img src='roomicon.png' className='h-[34px]'></img>
