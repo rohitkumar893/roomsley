3b824f162ee92eac1152b1c477423a87ffc95db4
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-05-22 08:27:13 +0530

    increased gap in nav

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
         <h1 className='text-white cursor-pointer' onClick={() => window.location.reload()}>Roomsley</h1>
         </nav>
 
-        <nav className='h-[105px] flex justify-center items-center gap-[28px] md:gap-[60px]'>
+        <nav className='h-[105px] flex justify-center items-center gap-[30px] md:gap-[60px]'>
             <ul><h2 className='homebtn text-[18px] hover:scale-110 text-gray-800 font-medium' onClick={() => window.location.reload()}>HOME</h2></ul>
             <ul><Link to="/login"><h2 className='text-[18px] hover:scale-110 text-gray-800 font-medium'>LOGIN</h2></Link></ul>
             <ul><Link to='/signup'><h2 className='text-[18px] hover:scale-110 text-gray-800 font-medium'>SIGNUP</h2></Link></ul>
