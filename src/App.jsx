20d8211e9a38bca0a4aedc76ad5117065001c5a2
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-05-24 09:55:00 +0530

    improved responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -68,8 +68,8 @@ function App() {
           <div className='padder h-[100%] w-[100%]'>
             <div className='marginctrl flex flex-col md:flex-row h-full w-full justify-between gap-[45px]'>
               <div className='flex flex-col gap-[30px]'>
-                  <h1 className='textlogo text-[50px] w-[300px] md:text-[60px] font-bold leading-tight tracking-wide'>Find Perfect<br />Room<span className='animate-pulse'>.</span></h1>
-                  <h2 className='text-[16px] sm:w-[410px] w-[360px] font-medium'> Whether you're finding a place or offering one, Roomsley makes it simple and quick.</h2>
+                  <h1 className='textlogo text-[40px] w-[300px] md:text-[60px] font-bold leading-tight tracking-wide'>Find Perfect<br />Room<span className='animate-pulse'>.</span></h1>
+                  <h2 className='text-[18px] sm:w-[410px] w-[360px] font-medium text-gray-600'> Whether you're finding a place or offering one, Roomsley makes it simple and quick.</h2>
                   <Link to="/listings">
                   <button className="buttonn w-[180px]">
                   <img src='roomicon.png' className='h-[34px]'></img>
