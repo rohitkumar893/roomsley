f9bc2c5ded47a1e89ff90a29a5ca3e15acf3107b
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-05-31 12:38:49 +0530

    removed fixed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
   return (
     <>
       <div className="first w-[100%]">
-      <nav className="h-[68px] w-full flex justify-between items-center bg-gray-800 text-[22px] sticky top-0 z-50">
+      <nav className="h-[68px] w-full flex justify-between items-center bg-gray-800 text-[22px] z-50">
         <div className='w-full flex justify-start items-center gap-[8px]'>
           <img src='door.png' className='homeicon h-[35px]'></img>
           <h1 className='text-white cursor-pointer' onClick={() => window.location.reload()}>Roomsley</h1>
