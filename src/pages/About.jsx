65404f677b3d63f7dd15505d7966e1dc539deb3d
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-05-31 23:02:46 +0530

    changed text size for about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,11 +5,11 @@ const About = () => {
     <div className='aboutpage w-full flex justify-center items-center'>
         <div className='aboutcontainer flex flex-col gap-[10px] rounded-3xl'>
             <h1 className='text-[25px] sm:text-[32px] font-semibold flex justify-center items-center text-gray-800'>ABOUT</h1>
-        <p className='text-[16px] sm:text-[20px] font-small'> <span className='font-semibold'>Find, List, and Rent with Ease :</span><br />
+        <p className='text-[18px] sm:text-[20px] font-small'> <span className='font-semibold'>Find, List, and Rent with Ease :</span><br />
 
             We offer a seamless experience with a wide range of verified listings tailored for students, professionals, and newcomers to any city. Every listing is pre-screened for safety, affordability, and comfort—eliminating the hassle of endless searching and unreliable posts.<br /><br />
 
-            <span className='font-semibold'>List Your Property with Confidence :</span><br />
+            <span className='font-semibold'>List Property with Confidence :</span><br />
 
             Turn your spare room or property into steady income by listing it directly on our platform - no brokers, no hidden fees. Our user-friendly listing process lets you upload photos and set your price. We connect you with genuine tenants, helping you rent faster and more securely.<br /><br />
 
