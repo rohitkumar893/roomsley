e84b7f51d499c79ccd4a9169ba3636524154ad86
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-06-03 09:17:18 +0530

    modified divider

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ function App() {
       
 
       <div className='second flex flex-col gap-[100px]'>
-        <img src="divider.png" className='divider w-[500px] h-[30px]'></img>
+        <img src="divider.png" className='divider w-[500px] h-[20px]'></img>
         <h1 className='services rounded-xl text-[35px] sm:text-[40px] text-center text font-semibold text-white bg-gray-800 z-5'>SERVICES</h1>
 
         <div className='findrooms flex flex-col sm:flex-row gap-[50px] w-full rounded-[15px] shadow-lg shadow-gray-600'>
