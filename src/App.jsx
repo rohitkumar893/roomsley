49e9233c0740c41b49079f07fa588c29da8cbffc
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-06-16 17:58:33 +0530

    added search bar

--- src/App.jsx
+++ src/App.jsx
@@ -113,6 +113,26 @@ function App() {
             )}
           </nav>
 
+          <div className='w-full flex justify-center'>
+            <div className="searchh w-full flex justify-center items-center">
+                <input placeholder="Search" class="search__input" type="text" />
+                <button class="search__button">
+                  <svg
+                    viewBox="0 0 16 16"
+                    class="bi bi-search"
+                    fill="currentColor"
+                    height="16"
+                    width="16"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+
         <div className='landingcontainer h-[100%] w-[100%] flex flex-col sm:flex-row justify-center items-center'>
           <div className='padder h-[100%] w-[100%]'>
             <div className='marginctrl flex flex-col md:flex-row h-full w-full justify-between gap-[45px]'>
